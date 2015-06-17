d93763a61f775912ac000035c2f92a03d6820410
Jonathan Ribas <jonathan@uecommerce.com.br>
2015-06-17 22:51:57 +0200

    change dias_validade_boleto setting

--- app/code/community/Uecommerce/Mundipagg/sql/mundipagg_setup/mysql4-upgrade-2.0.1-2.0.2.php
+++ app/code/community/Uecommerce/Mundipagg/sql/mundipagg_setup/mysql4-upgrade-2.0.1-2.0.2.php
@@ -34,9 +34,9 @@
 
     Mage::getConfig()->saveConfig('payment/mundipagg_standard/apiUrlStaging','https://stagingv2.mundipaggone.com/Sale/');
     Mage::getConfig()->saveConfig('payment/mundipagg_standard/apiUrlProduction','https://transactionv2.mundipaggone.com/Sale/');
-    Mage::getConfig()->saveConfig('payment/mundipagg_boleto/dias_validade_boleto','5');
     Mage::getConfig()->reinit();
     Mage::getConfig()->cleanCache();
     Mage::app()->reinitStores();
 
     $installer->endSetup();
+    
