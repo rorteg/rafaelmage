8e40131d0abfe6fe2fec56188a8e1af6eeb39513
Rafael Ortega Bueno <rafael@uecommerce.com.br>
2015-07-27 01:27:32 -0300

    Added sleep default

--- app/code/community/Uecommerce/Mundipagg/Test/Selenium/BoletoTest.php
+++ app/code/community/Uecommerce/Mundipagg/Test/Selenium/BoletoTest.php
@@ -28,19 +28,19 @@ class Uecommerce_Mundipagg_Test_Selenium_BoletoTest extends Uecommerce_Mundipagg
     
     protected function setBoleto(){
         $this->clickButtonByContainer('shipping-method-buttons-container');
-        sleep(1);
+        sleep(self::$_defaultSleep);
         $this->byId('p_method_mundipagg_boleto')->click();
         $this->byId('mundipagg_boleto_boleto_taxvat')->value(self::$_custmerTest['taxvat']);
         $this->clickButtonByContainer('payment-buttons-container');
-        sleep(1);
+        sleep(self::$_defaultSleep);
         $this->clickButtonByContainer('review-buttons-container');
-        sleep(20);
+        sleep(self::$_defaultSleep);
         
         $elements = $this->byCssSelector('.pagSucess')->elements($this->using('css selector')->value('a'));
         foreach($elements as $element){
             $element->click();
         }
-        sleep(5);
+        sleep(self::$_defaultSleep);
         $this->assertContains('mundipagg',$this->url());
         
     }
