c93f8636f64f9b6cf4bf7f6bdc378d085d890224
Rafael Ortega Bueno <rafael@uecommerce.com.br>
2015-07-28 16:19:33 -0300

    bug found in unit testing, several Hipercard cards were not going through.

--- js/uecommerce/mundipagg.js
+++ js/uecommerce/mundipagg.js
@@ -173,7 +173,7 @@ function checkCredcardType(cardNumber){
     var regexAmex = /^3[47][0-9]{13}/;
     var regexDiners = /^3(?:0[0-5]|[68][0-9])[0-9]{11}/;
     var regexElo = /^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$/;
-    var regexHipercard = /^(3841\d{10}(\d{3})?)|(3841\d{15})$/;
+    var regexHipercard = /^(606282\d{10}(\d{3})?)|(3841\d{15})$/;
     //var regexDiscover = /^6(?:011|5[0-9]{2})[0-9]{12}/;
     //var regexJCB = /^(?:2131|1800|35\d{3})\d{11}/;
 
